0c47cc1ce8a16e2fbc427d5e0f47e1329f1dd688
Greg Hogan <me@greghogan.uk>
2023-04-15 12:26:29 +0100

    Fix some TS issues and complete morse map
    - Update method for adding controllers to global controller
      state.
    - Update the morse map list to include all numbers.
    - Fix some TS issues.

--- src/App.tsx
+++ src/App.tsx
@@ -12,71 +12,63 @@ function App() {
   const haveEvents = "GamepadEvent" in window;
 
   const [morseText, setMorseText] = useState("");
-  const [message, setMessage] = useState("I love Olga");
+  const [message, setMessage] = useState("aa bb");
   const [running, setRunning] = useState(false);
-  const [isControllerConnected, setIsControllerConnected] = useState("");
-  const [controllers, setControllers] = useState([]);
+  const [isControllerConnected, setIsControllerConnected] = useState(false);
+  const [controllers, setControllers] = useState<GamepadCustom[]>([]);
 
   const assignConnectedControllers = () => {
-    const gamepads = navigator?.getGamepads() ?? [];
-    // @ts-ignore jog on
-    const newGP = gamepads
-      // @ts-ignore jog on
-      .reduce((prev, curr) => {
-        return [...prev, curr];
-      }, [])
-      // @ts-ignore jog on
-      .filter(Boolean);
+    const detectedGamepads = (navigator?.getGamepads().filter(Boolean) ??
+      []) as NonNullable<GamepadCustom>[];
 
-    setControllers(newGP);
+    setControllers(detectedGamepads);
 
-    newGP.map(async (controller: GamepadCustom) => {
-      vibrateControllerConnected({ controller });
-    });
-  }
+    if (!!detectedGamepads.length) {
+      detectedGamepads.map(async (controller: GamepadCustom) => {
+        vibrateControllerConnected({ controller });
+      });
+    }
+  };
 
   const connectHandler = () => {
     console.warn("connected");
+    setIsControllerConnected(true);
     assignConnectedControllers();
   };
 
   const disconnectHandler = () => {
+    setIsControllerConnected(false);
     console.warn("disconnectHandler");
   };
 
   const scanGamePads = () => {
-    const gamepads = navigator?.getGamepads() ?? [];
-    console.info("gamepads", gamepads);
+    const connectedGamepads = navigator?.getGamepads() ?? [];
 
-    if (gamepads.length) {
-      // @ts-ignore jog on
-      const newGP = gamepads
-        // @ts-ignore jog on
+    if (connectedGamepads.length) {
+      const detectedGamepads = connectedGamepads
+        .filter(Boolean)
         .reduce((prev, curr) => {
           return [...prev, curr];
-        }, [])
-        // @ts-ignore jog on
-        .filter(Boolean);
+        }, []);
 
-      // @ts-ignore jog on
-      setControllers(newGP);
+      setControllers(detectedGamepads);
     }
   };
 
   const pause = async (durationMS: number) =>
     new Promise((resolve, _reject) => setTimeout(resolve, durationMS));
 
-    const spaceMorseEvent = () => pause(400);
-    const spaceLetters = async () => {
-      setMorseText((currentMorseText) => `${currentMorseText} `);
-      await pause(950);
-    };
-    const spaceWord = async () => {
-      await pause(950);
-      console.log(' / ')
-      setMorseText((currentMorseText) => `${currentMorseText} / `);
-      await pause(950);
-    };
+  const spaceMorseEvent = () => pause(400);
+  const spaceLetters = async () => {
+    await pause(950);
+    setMorseText((currentMorseText) => `${currentMorseText} `.trim());
+  };
+  const spaceWord = async () => {
+    // await pause(950);
+    console.log(" / ");
+    setMorseText((currentMorseText) => `${currentMorseText} / `);
+    await pause(950);
+  };
 
   const morseCharacter = async ({
     controller,
@@ -85,7 +77,7 @@ function App() {
     controller: GamepadCustom;
     character: string;
   }) => {
-    console.log(character)
+    console.log(character);
     const morseFragments = getMorseCharacterFragments({ character });
 
     for (const [index, fragment] of morseFragments.entries()) {
@@ -103,7 +95,6 @@ function App() {
       await morseDot({ controller });
     }
   };
-
 
   const playBackMorseMessage = async () => {
     const morseMap = message.split("");
@@ -136,7 +127,7 @@ function App() {
     evt.preventDefault();
 
     playBackMorseMessage();
-  }
+  };
 
   const handleOnChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(evt.target.value);
--- src/utils/getMorseCharacterFragments.ts
+++ src/utils/getMorseCharacterFragments.ts
@@ -93,6 +93,27 @@ export const getMorseCharacterFragments = ({
     case "2":
       return [".", ".", "-", "-", "-"];
 
+    case "3":
+      return [".", ".", ".", "-", "-"];
+
+    case "4":
+      return [".", ".", ".", ".", "-"];
+
+    case "5":
+      return [".", ".", ".", ".", "."];
+
+    case "6":
+      return ["-", ".", ".", ".", "."];
+
+    case "7":
+      return ["-", "-", ".", ".", "."];
+
+    case "8":
+      return ["-", "-", "-", ".", "."];
+
+    case "9":
+      return ["-", "-", "-", "-", "."];
+
     default:
       return [];
   }
